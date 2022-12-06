ac4b765ee42fcdb6ea5664735a6cc53218859008
rageofgods <rageofgods@gmail.com>
2022-12-06 10:57:32 +0300

    refactor: redone 'return' statement in try/except section if __get_comp func

--- nexus_parser.py
+++ nexus_parser.py
@@ -30,8 +30,9 @@ class NexusParser:
         else:
             http_request = f'{srv_url}{self.__repo_comp_urn(continuation_token)}'
 
+        response = None
         try:
-            return self.pool_manager.request('GET', http_request, headers=self.http_headers)
+            response = self.pool_manager.request('GET', http_request, headers=self.http_headers)
         except http_ex.MaxRetryError:
             logging.error(f'Max retries exceeded while connecting to server {srv_url}')
         except http_ex.ConnectTimeoutError:
@@ -40,6 +41,8 @@ class NexusParser:
             logging.error(f'HTTP error occurred while connecting to server {srv_url}')
         except Exception as e:
             logging.error(f'Non specific http request error: {e}')
+
+        return response
 
     def setup_headers(self):
         if self.server_user == '' and self.server_pass == '':
